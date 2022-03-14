50c741ffb6718b9adba06ae9b0e78e1f17b3c67c
popchanskiy <popov.ilya1240@gmail.com>
2022-03-14 22:26:01 +0300

    add configuration

--- src/test/java/lesson9/tests/BaseTest.java
+++ src/test/java/lesson9/tests/BaseTest.java
@@ -1,20 +1,28 @@
 package lesson9.tests;
 
 import com.codeborne.selenide.*;
+import com.codeborne.selenide.logevents.*;
+import io.qameta.allure.selenide.*;
 import lesson9.pages.forms.practice_form_page.*;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.*;
 
 public class BaseTest {
-    StudentRegistrationFormPage automationPracticeForm= new StudentRegistrationFormPage();
-    SubmittingReportPage submittingReportPage=new SubmittingReportPage();
+    StudentRegistrationFormPage automationPracticeForm = new StudentRegistrationFormPage();
+    SubmittingReportPage submittingReportPage = new SubmittingReportPage();
 
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = false;
-        Configuration.headless = false;
-        //Configuration.browserSize="1900*108";
-        Configuration.browser = "chrome";
-        Configuration.browserSize ="1920x1080"; //1920x1080
+        Configuration.browserSize = "1920x1080";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
     }
+
 }
